074d8884a804a615d2eca0835dc714cba5afaa5a
Fidan Zeynalli <fidanzeynalli30@gmail.com>
2026-04-12 16:56:50 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,46 @@
-require('dotenv').config();
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { graphqlUploadExpress } = require('graphql-upload-minimal');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { graphqlUploadExpress } = require("graphql-upload-minimal");
+const cors = require("cors");
 
-const connectDB = require('./config/db');
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const getUser = require('./utils/auth');
+const connectDB = require("./config/db");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const getUser = require("./utils/auth");
 
 const app = express();
 
 app.use(cors());
 app.use(graphqlUploadExpress());
 
+let serverStarted = false;
+
 async function startServer() {
-    await connectDB();
+  if (serverStarted) return app;
 
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-        uploads: false,
-        context: ({ req }) => {
-            const token = req.headers.authorization?.split(' ')[1];
-            const user = getUser(token);
-            return { user };
-        }
-    });
+  await connectDB();
 
-    await server.start();
-    server.applyMiddleware({ app, path: '/graphql' });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    uploads: false,
+    context: ({ req }) => {
+      const token = req.headers.authorization?.split(" ")[1];
+      const user = getUser(token);
+      return { user };
+    },
+  });
 
-    app.listen(5000, () =>
-        console.log(`Server running at http://localhost:5000/graphql`)
-    );
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  serverStarted = true;
+
+  return app;
 }
 
-startServer().catch(err => {
-    console.error("Server failed to start:");
-    console.error(err);
-});
+module.exports = async (req, res) => {
+  const app = await startServer();
+  return app(req, res);
+};
